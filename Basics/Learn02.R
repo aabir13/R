68e3cebfac90d0d5291af5081e4f82ca6505584e
Aabir Datta <dattaaabir13@gmail.com>
2021-10-13 02:36:44 +0530

    Create Learn02.R

---
+++ Basics/Learn02.R
@@ -0,0 +1,24 @@
+#Arithmetic operations in R
+
+#Order of Operations:
+#1. Parenthesis()
+#2. Exponent(^)
+#3. Mul and Div(Left to Right)
+#4. Add and Sub(Left to Right)
+
+a <- 100+100
+a
+b <- 100-50
+b
+c <- 100*2
+c
+d <- 100/2
+d
+e <- 3%%2 
+e
+
+#Varying answers following different order of operations
+result1 = 34+46/2
+result2 = (34+46)/2
+result1
+result2

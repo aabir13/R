5d76c1cec0665ffe6f7789cb27513c2b7cf2e7cc
Aabir Datta <dattaaabir13@gmail.com>
2021-10-13 02:35:30 +0530

    Create Learn01.R

---
+++ Basics/Learn01.R
@@ -0,0 +1,23 @@
+#Variables and data types in R
+
+model1 <- 'Aabir'
+#returns the type of the variable
+typeof(model1)
+#returns the class to which the variable belongs
+class(model1)
+model1
+
+x <- 10
+typeof(x)
+class(x)
+x
+
+y <- 100L
+typeof(y)
+class(y)
+y
+
+a=TRUE
+typeof(a)
+class(a)
+a

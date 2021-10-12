c685b0e12244c341ae8bc7c1af67cb2127dcf4de
Aabir Datta <dattaaabir13@gmail.com>
2021-10-13 02:45:10 +0530

    Create Learn05.R

---
+++ Basics/Learn05.R
@@ -0,0 +1,14 @@
+#Print formatting in R
+
+var <- 11
+print(var)
+
+#Creating integer sequences
+x <- 10:30
+print(x)
+class(x)
+
+print(paste('aabir','datta'))
+print(paste('aabir','datta',sep = '-'))
+paste0('aabir','datta')
+sprintf("%s is %i years old","Aabir",21)

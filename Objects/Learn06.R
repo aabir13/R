7adf6605ff5bc6e63708aa0a955c981dc14e09a8
Aabir Datta <dattaaabir13@gmail.com>
2021-10-15 22:46:31 +0530

    Create Learn06.R

---
+++ Objects/Learn06.R
@@ -0,0 +1,18 @@
+a <- 1
+ai <- 2L
+#Inf: Represents infinity
+1/Inf
+typeof(a)
+typeof(ai)
+
+#Initializing a vector
+v1 <- c(1,2,3,4,5)
+v1
+v2 <- c('aabir','gauri')
+print(v2)
+class(v1)
+class(v2)
+
+v3 <- c(TRUE, FALSE)
+v3
+class(v3)
